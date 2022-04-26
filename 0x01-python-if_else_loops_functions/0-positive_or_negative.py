b83ec8c947007eeb57e16e2aaeeb8a901360e673
Xavier32 <xavierperez1599@gmail.com>
2022-04-26 12:51:33 -0400

    adding task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 from traceback import print_tb
 number = random.randint(-10, 10)
 if number > 0:
-    print("{:d} is positive\n".format(number))
+    print("{} is positive\n".format(number))
 elif number < 0:
-    print("{:d} is negative\n".format(number))
+    print("{} is negative\n".format(number))
 else:
-    print("{:d} is negative\n".format(number))
+    print("{} is zero\n".format(number))
