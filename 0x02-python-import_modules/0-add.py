16d4488334fa12426ca405c3072b813dadb59bdf
Xavier32 <xavierperez1599@gmail.com>
2022-04-28 18:16:42 -0400

    fixing task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,8 +5,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    add = a + b
-    print("{} + {} = {}".format(a, b, add))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
