95680fb823cee3f05b68e5efb0a6c54bfff50ad6
Xavier32 <xavierperez1599@gmail.com>
2022-04-28 19:02:20 -0400

    adding task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv)
     if num == 1:
-        print("{} arguments.".format(num - 1))
+        print("{:d} arguments.".format(num - 1))
     elif num == 2:
-        print("{} argument:".format(num - 1))
+        print("{:d} argument:".format(num - 1))
     else:
-        print("{} argument:".format(num - 1))
+        print("{:d} argument:".format(num - 1))
     for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
