2f186fc97028a035f0fa75f24a71a8deaced30eb
Xavier32 <xavierperez1599@gmail.com>
2022-05-02 22:04:10 -0400

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(list_a) < 2:
         for i in range(len(list_a), 2):
             list_a.append(0)
-    
+
     list_b = list(tuple_b)
     if len(list_b) < 2:
         for i in range(len(list_b), 2):
