32bf4aa46d47c22a95e378e96a589669d03a2df6
Xavier32 <xavierperez1599@gmail.com>
2022-05-02 22:14:01 -0400

    adding task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def max_integer(my_list=[]):
+    cnt = len(my_list)
+    if cnt == 0:
+        return None
+    v_max = my_list[0]
+    for i in my_list:
+        if i > v_max:
+            v_max = 1
+    return v_max
