85abbad3309375325797dfcad5d3df079ed61425
Xavier32 <xavierperez1599@gmail.com>
2022-05-16 14:58:50 -0400

    fixed pycode style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
     for j in range(x):
         try:
             print("{}".format(my_list[j]), end="")
-        except:
+        except my_list:
             print("")
             return lenght
         cnt += 1
