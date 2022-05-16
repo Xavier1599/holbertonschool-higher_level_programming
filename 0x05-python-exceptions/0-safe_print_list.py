404b651ee1501273591bd576da23998d28984ded
Xavier32 <xavierperez1599@gmail.com>
2022-05-16 14:55:08 -0400

    adding task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    lenght = 0
+    cnt = 0
+    for i in my_list:
+        lenght += 1
+    for j in range(x):
+        try:
+            print("{}".format(my_list[j]), end="")
+        except:
+            print("")
+            return lenght
+        cnt += 1
+    print("")
+    return cnt
