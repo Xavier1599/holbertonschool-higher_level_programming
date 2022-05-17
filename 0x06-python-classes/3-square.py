31666ab56f701b29ec4f70941729208402b488c8
Xavier <xavierperez1599@gmail.com>
2022-05-17 18:11:07 -0400

    adding task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defining the square"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize the square
+    
+        size: the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the area square"""
+        return (self.__size * self.size)
