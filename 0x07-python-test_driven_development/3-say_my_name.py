008c0ff7bbb7b13b4fbb699aca06389001c57f16
Xavier <xavierperez1599@gmail.com>
2022-05-22 16:22:58 -0400

    adding task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""File name: 3-say_my_name.py
+Function that prints My name is <first name> <last name>
+Prototype: def say_my_name(first_name, last_name=""):
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Return the full printed name
+    first_name and Last_name must be strings
+    Raise:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
