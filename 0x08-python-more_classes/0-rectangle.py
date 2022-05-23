9900c566a29e16e755e632f8dd33e5b3da4ec3ae
Xavier <xavierperez1599@gmail.com>
2022-05-23 13:52:05 -0400

    adding task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""File name: 0-rectangle.py
+Rectangel: a simple class defining a rectangle
+"""
+
+
+class Rectangle(object):
+    """defining a new class called rectangle"""
+    pass
