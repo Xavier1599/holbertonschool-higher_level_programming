c1aa3e29a7d8b6dea8695d1623e0d72f79d8e3c9
Xavier <xavierperez1599@gmail.com>
2022-05-23 20:18:26 -0400

    adding task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""File name: 3-rectangle.py
+Rectangle: defining its class and atributes
+"""
+
+
+class Rectangle:
+    """represent the rectangle"""
+    def __init__(self, width=0, height=0):
+        """initialize the rectsangle
+
+        Args:
+            width: the width of the rectangle
+            heigth: the heigth of the triangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """set the heigth of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """return the primeter of the rectangle"""
+        if self.__width == 0 or self.height == 0:
+            return(0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """return the representation of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
