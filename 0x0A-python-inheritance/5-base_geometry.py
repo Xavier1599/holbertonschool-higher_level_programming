6295b99e59ce89a4e833a800a8ed6393c0eb6f43
Xavier <xavierperez1599@gmail.com>
2022-05-30 17:44:45 -0400

    adding task 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""File name: 5-base_geometry.py"""
+
+
+class BaseGeometry(object):
+    """define class bas geometry"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""File name: 6-base_geometry.py"""
+
+
+class BaseGeometry(object):
+    """define class base geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
