1ad116b77ecc39bf914db1e5911a0b89cb30ce39
Xavier <xavierperez1599@gmail.com>
2022-05-30 20:52:57 -0400

    adding task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""File name: 11-square.py"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square inherence"""
+    def __init__(self, size):
+        """initialize the square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
