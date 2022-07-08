8069e082c308122d17e008086a7a61879a6f6e35
Xavier <xavierperez1599@gmail.com>
2022-07-07 21:05:07 -0400

    15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,7 @@ FROM tv_shows AS t
 INNER JOIN tv_show_genres AS s
 ON t.id = s.show_id
 
-INNER JOIN tv_shows AS g
+INNER JOIN tv_genres AS g
 ON t.id = s.genre_id
 WHERE g.name = "Comedy"
 ORDER BY t.title;
