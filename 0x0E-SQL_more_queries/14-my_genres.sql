efb2ef5a8bfbadd4b9ad3149c82569f9afe3315e
Xavier <xavierperez1599@gmail.com>
2022-07-07 20:51:37 -0400

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- script that uses the hbtn_0d_tvshows database
-SELECT tv_genres.name name
-FROM tv_genres
-JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show
-ON tv_show.id = tv_show_genres.show_id
-WHERE tv_shows.title LIKE %Dexter%
-ORDER BY tv_genres.name ASC;
+SELECT g.name
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS s
+ON g.id = s.genre_id
+INNER JOIN tv_show AS t
+ON t.id = s.show_id
+WHERE t.title = Dexter
+ORDER BY g.name;
