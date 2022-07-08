0444776dae6b713b9018b68f32db01971f2c693b
Xavier <xavierperez1599@gmail.com>
2022-07-07 20:56:44 -0400

    awdhu8cdfophsbogbjo

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@ FROM tv_genres AS g
 INNER JOIN tv_show_genres AS s
 ON g.id = s.genre_id
 
-INNER JOIN tv_show AS t
+INNER JOIN tv_shows AS t
 ON t.id = s.show_id
 WHERE t.title = "Dexter"
 ORDER BY g.name;
