8940bf8174146436ad8c5adcd33553f2fb2efaf5
XavierPerezz <xnator9@gmail.com>
2022-08-03 01:00:36 +0000

    adding task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-const SquareBase = require('./5-square');
-
-class Square extends SquareBase {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    process.stdout.write((c.repeat(this.width) + '\n').repeat(this.height));
-  }
-}
-
-module.exports = Square;
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
+};
