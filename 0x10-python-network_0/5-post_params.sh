14b8049164f49278431ca377911e12847eddbd94
XavierPerezz <xnator9@gmail.com>
2022-08-23 18:02:21 +0000

    fixed wc error

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL POST parameters 
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Send POST request
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
