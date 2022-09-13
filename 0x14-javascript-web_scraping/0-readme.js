3075d533f5e6cc458907ba9887c104268f243574
XavierPerezz <xnator9@gmail.com>
2022-09-13 17:36:59 +0000

    adding task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,11 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', function read(err, data) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    const content = data;
-    console.log(content);
+fs.readFile(file, 'utf8', function read (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const content = data;
+  console.log(content);
 });
-
